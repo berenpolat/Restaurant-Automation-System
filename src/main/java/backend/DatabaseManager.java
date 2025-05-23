c6aff05a649f008a9f909cc50883fd2ab191043d
melikeaytac <48melikeaytac@gmail.com>
2025-05-23 23:22:11 +0300

    database changes

--- src/main/java/backend/DatabaseManager.java
+++ src/main/java/backend/DatabaseManager.java
@@ -5,7 +5,7 @@ import java.sql.*;
 public class DatabaseManager {
     private static final String URL = "jdbc:mysql://localhost:3306/restaurant";
     private static final String USER = "root";
-    private static final String PASS = "677610Ay";
+    private static final String PASS = "password";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASS);
